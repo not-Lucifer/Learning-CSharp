b2a6021ba98df7ac96041f2768df98ea6ec5a6f1
Aman Singh <amansingh.makemeup@gmail.com>
2025-10-23 12:04:13 +0530

    changes

---
+++ 6.Collections.cs
@@ -0,0 +1,6 @@
+using System;
+int[] array = [1, 2, 3, 4, 5];
+// 2nd method
+int[] array1 = new int[5]; //initializes array with default value 0;
+Console.WriteLine(array1[0]);
+Console.WriteLine(array[0]);
