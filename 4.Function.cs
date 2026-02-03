a37b87271e4a0a26b9373d14886f906255f4155c
Aman Singh <amansingh.makemeup@gmail.com>
2026-02-03 09:22:04 +0530

    changes

--- 4.Function.cs
+++ 4.Function.cs
@@ -2,4 +2,9 @@ using System;
 static void TestFunction(){
 	Console.WriteLine("Message");
 }
+void hello(string playername)
+{
+	Console.WriteLine($"Hello {playername}");
+}
 TestFunction();
+hello("Aman");
