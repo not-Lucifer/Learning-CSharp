ba66fa3ea0ab7af45181ddfa895ddc96f80f284a
Aman Singh <amansingh.makemeup@gmail.com>
2026-03-01 22:51:45 +0530

    Changes

--- 18.multidimensionalarray.cs
+++ 18.multidimensionalarray.cs
@@ -1,4 +1,6 @@
 //2D ARRAY
+using System.Diagnostics;
+
 int[,] intarray = new int[5,6];
 intarray[0,2]=6;
 Console.WriteLine(intarray[0,2]);
@@ -9,6 +11,32 @@ for(int i = 0 ; i < array2.GetLength(0);i++)
     for(int j = 0; j < array2.GetLength(1); j++)
     {
         Console.Write(array2[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+//2D Jagged array(array of array)
+
+int[][] array3 = new int[3][];
+array3[0]= new int[3];
+array3[1]= new int[3];
+array3[2]= new int[3];
+array3[0][0] = 1;
+array3[0][1] = 1;
+array3[0][2] = 1;
+array3[1][0] = 1;
+array3[1][1] = 1;
+array3[1][2] = 1;
+array3[2][0] = 1;
+array3[2][1] = 1;
+array3[2][2] = 1;
+Console.WriteLine();
+
+for(int i = 0; i < array3.Length; i++)
+{
+    for(int j = 0; j < array3[0].Length; j++)
+    {
+        Console.Write(array3[i][j]+" ");
     }
     Console.WriteLine();
 }
