c7d0f5f5bafca485a95658559bdbebfcb366682f
Aman Singh <amansingh.makemeup@gmail.com>
2026-03-01 21:59:34 +0530

    Changes

---
+++ 17.property.cs
@@ -0,0 +1,13 @@
+private static string playername
+{
+    get
+    {
+        return playername;
+    }
+    set
+    {
+        playername = value;
+    }
+}
+playername = "Not-Lucifer";
+Console.WriteLine(playername);
